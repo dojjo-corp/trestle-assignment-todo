c7b0b5888b9ade317e879e2bee591d99123596df
dojjo-termux <hosuatetteh@gmail.com>
2023-07-13 19:15:26 +0000

    theme toggling -i

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 const page = document.body;
+
+// get css variables for theme toggling
+const root = document.querySelector(":root");
+const rootVariables = window.getComputedStyle(root);
+const bgColorDark = rootVariables.getPropertyValue("--dark-bgcolor");
+const bgColorLight = rootVariables.getPropertyValue("--light-bgcolor");
+const bgImageDark = rootVariables.getPropertyValue("--dark-bg-image");
+const bgImageLight = rootVariables.getPropertyValue("--light-bg-image");
+const textColorDark = rootVariables.getPropertyValue("--dark-color-text");
+const textColorLight = rootVariables.getPropertyValue("--light-color-text");
+
 const elementsTextChamgedByTheme = [];
-const
+
 var darkThemeToggle = document.querySelector("dark");
 darkThemeToggle.addEventListener("click", function(){
+
+  // set dark baxkheound 
+  page.style.backgroundImage = bgImageDark;
   page.childElements.forEach(function(elem){
-    elem.classList.add("dark-theme");
+    
   })
 })
