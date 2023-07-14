faeb7777dfe71feb5abd86b14d14aafcbff0a9cd
dojjo-termux <hosuatetteh@gmail.com>
2023-07-14 17:23:06 +0000

    black beauty <3

--- script.js
+++ script.js
@@ -11,13 +11,34 @@ const textColorDark = rootVariables.getPropertyValue("--dark-color-text");
 const textColorLight = rootVariables.getPropertyValue("--light-color-text");
 
 const elementsTextChamgedByTheme = [];
-
-var darkThemeToggle = document.querySelector("dark");
+const todoList = document.querySelector(".todo-list");
+const newTodo = document.querySelector(".item-container");
+const newTodoText = document.querySelector("#new-todo");
+const lightThemeToggle = document.querySelector("#light");
+const darkThemeToggle = document.querySelector("#dark");
 darkThemeToggle.addEventListener("click", function(){
-
+  
+  // remove moon and set sun icon
+  darkThemeToggle.style.display = "none";
+  lightThemeToggle.style.display = "inline";
+  
   // set dark baxkheound 
-  page.style.backgroundImage = bgImageDark;
+  page.classList.add("dark");
+  todoList.classList.add("dark");
+  newTodo.classList.add("dark");
+  newTodoText.classList.add("dark");
   page.childElements.forEach(function(elem){
     
   })
+})
+
+lightThemeToggle.addEventListener("click", function() {
+  // remove moon and set sun icon
+  darkThemeToggle.style.display = "inline";
+  lightThemeToggle.style.display = "none";
+  
+  page.classList.remove("dark");
+  todoList.classList.remove("dark");
+  newTodo.classList.remove("dark");
+  newTodoText.classList.remove("dark");
 })
